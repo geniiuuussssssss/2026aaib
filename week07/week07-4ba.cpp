1406e1f3c579052c99c2c6e25e2890e879f8575e
geniiuuussssssss <14750295@me.mcu.edu.tw>
2026-04-08 11:36:06 +0800

    week07

---
+++ week07/week07-4ba.cpp
@@ -0,0 +1,10 @@
+//week07-ba.cpp SOIT107_BASE_007
+//method 2: char c
+#include <stdio.h>
+int main()
+{
+	char a,b,c,d,e;//char c1,c2,c3,c4,c5;
+	scanf("%c%c%c%c%c", &a, &b, &c, &d, &e);
+	printf("%c   %c   %c   %c   %c", a,b,c,d,e);
+
+}
